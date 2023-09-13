5462bf7fd0de68d889345c72d010b180b3fa9a0b
Oviglo <ovigne.loic@gmail.com>
2023-09-13 12:16:26 +0200

    liste

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Game;
+use App\Repository\GameRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class GameController extends AbstractController
 {
+    #[Route('/game')]
+    public function index(GameRepository $repository): Response
+    {
+        $entities = $repository->findAll(); // SELECT * FROM game
+
+        return $this->render('game/index.html.twig', [
+            'entities' => $entities, // Envois de tous les jeux dans le vue
+        ]);
+    }
+
     // Injection de dépendance: SF va m'envoyer les objets dont j'ai besoin en paramètre
-    #[Route('/game', name: 'app_game')]
-    public function index(EntityManagerInterface $entityManager): Response
+    #[Route('/game/new')]
+    public function new(EntityManagerInterface $entityManager): Response
     {
         $entity = new Game();
-        $entity->setName('Metroid');
+        $entity->setName('Megaman');
         $entity->setDescription('');
 
         // Indique à Doctrine de prendre en charge cet objet
